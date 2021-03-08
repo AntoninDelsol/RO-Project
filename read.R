32c87ffa4e7ebd48557e0ff1c037feba9c4cf55b
stanreynaud <60963901+stanreynaud@users.noreply.github.com>
2021-03-08 15:20:06 +0100

    fonction eval

--- read.R
+++ read.R
@@ -1,7 +1,18 @@
+# Importation des données en skippant les deux premières lignes
 donnees <- read.table(file.choose(), sep=" ",skip=2)
+# Pour supprimer la dernière colonne (n°103) dont les valeurs sont NA 
+donnees <- donnees[,colSums(is.na(df))<nrow(df)]
 
 
+# Pour tester eval
+df <- donnees[c(1,2,3),]
 
-eval <- function(donnees,tableau_de_fournisseurs){
-  return ()
+# fournisseurs_choisis doit être un sous-ensemble de lignes de données (comme df)
+eval <- function(fournisseurs_choisis){
+  ouverture <- sum(fournisseurs_choisis[2],na.rm = TRUE)
+  cout_clients <- 0
+  for (j in 3:ncol(fournisseurs_choisis)) {
+    cout_clients <- cout_clients+min(fournisseurs_choisis[j])
+  }
+  return (ouverture+cout_clients)
 }
